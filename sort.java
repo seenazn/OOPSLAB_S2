5cdedac36a745d26765b3813b48f622ab0d6bfc2
Seena <167744692+seenazn@users.noreply.github.com>
2025-03-21 10:03:46 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.Arrays;
+class sort
+{
+public static void main(String args[])
+{
+int n,i,j;
+String temp;
+Scanner sc=new Scanner(System.in);
+Scanner st=new Scanner(System.in);
+System.out.println("Enter no.of strings");
+n=sc.nextInt();
+String[] str=new String[n];
+System.out.println("Enter the strings");
+for(i=0;i<n;i++)
+{
+str[i]=st.nextLine();
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if (str[i].compareTo(str[j])>0)
+{
+temp=str[i];
+str[i]=str[j];
+str[j]=temp;
+}
+}
+}
+System.out.println("sorted strings");
+for(i=0;i<n;i++)
+{
+System.out.println(str[i]);
+}
+}
+}
+
+
+
